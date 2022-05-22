f0d4ccefb88a5894de918129d668e6d1fb04d06c
KHP53 <khp4121@gmail.com>
2022-05-22 15:22:09 +0600

    added packages, added inital MVVM setup, added theme data

---
+++ lib/src/app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:todo_app/common/themes.dart';
+import 'settings/settings_controller.dart';
+
+/// The Widget that configures your application.
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required this.settingsController,
+  }) : super(key: key);
+
+  final SettingsController settingsController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return GetMaterialApp(
+          defaultTransition: kIsWeb ? Transition.fadeIn : Transition.native,
+          debugShowCheckedModeBanner: false,
+          //restorationScopeId: 'app',
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          onGenerateTitle: (BuildContext context) =>
+              AppLocalizations.of(context)!.appTitle,
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          themeMode: settingsController.themeMode,
+          home: HomeView(),
+        );
+      },
+    );
+  }
+}
