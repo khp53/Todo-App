fdeb2170ce60b35c5963f4b9c0a8aec7f640c00a
KHP53 <khp4121@gmail.com>
2022-05-22 16:25:19 +0600

    The following changes were made: - created custom text field. - added home viewmodel. - added bottom nav bar in home body.

--- lib/screens/home/home_viewmodel.dart
+++ lib/screens/home/home_viewmodel.dart
@@ -1,3 +1,49 @@
 import 'package:todo_app/screens/viewmodel.dart';
 
-class HomeViewmodel extends Viewmodel {}
+class HomeViewmodel extends Viewmodel {
+  static final HomeViewmodel _instance = HomeViewmodel._internal();
+  factory HomeViewmodel() {
+    return _instance;
+  }
+
+  HomeViewmodel._internal();
+
+  bool _isLoading = false;
+  bool _isCompleted = false;
+  int _selectedIndex = 0;
+
+  bool get isLoading => _isLoading;
+  set isLoading(bool value) {
+    _isLoading = value;
+    turnIdle();
+  }
+
+  bool get isCompleted => _isCompleted;
+  set isCompleted(bool value) {
+    _isCompleted = value;
+    turnIdle();
+  }
+
+  int get selectedIndex => _selectedIndex;
+  set selectedIndex(int value) {
+    _selectedIndex = value;
+    turnIdle();
+  }
+
+  // TextEditingController? _titleController;
+  // TextEditingController? _descriptionController;
+
+  // TextEditingController? get titleController => _titleController;
+
+  // set titleController(TextEditingController? value) {
+  //   _titleController = value;
+  //   turnIdle();
+  // }
+
+  // TextEditingController? get descriptionController => _descriptionController;
+
+  // set descriptionController(TextEditingController? value) {
+  //   _descriptionController = value;
+  //   turnIdle();
+  // }
+}
--- lib/screens/home/widgets/home_body.dart
+++ lib/screens/home/widgets/home_body.dart
@@ -7,8 +7,54 @@ class HomeBody extends StatelessWidget {
   final HomeViewmodel homeViewmodel;
   const HomeBody({Key? key, required this.homeViewmodel}) : super(key: key);
 
+  static const List<Widget> _widgets = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    homeViewmodel.selectedIndex = index;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var theme = Theme.of(context);
+    return Scaffold(
+      body: Center(
+        child: _widgets.elementAt(homeViewmodel.selectedIndex),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        onPressed: () {},
+        backgroundColor: theme.colorScheme.primary,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+        currentIndex: homeViewmodel.selectedIndex,
+        selectedItemColor: theme.colorScheme.primary,
+        unselectedItemColor: theme.colorScheme.secondary,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
